b4b2a20b90e1b6acd8241f8cd9da319947633a7e
OpenSauce <lawleycobhc@gmail.com>
2024-12-02 08:26:23 +0000

    Day one task one

---
+++ 1/1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+using namespace std; 
+
+int main()
+{
+    std::ifstream input("input.txt");
+    vector<int> v1;
+    vector<int> v2;
+    for (std::string line; getline(input, line);)
+    {
+        std::istringstream is(line);
+        int a, b;
+        is >> a >> b;
+        v1.push_back(a);  
+        v2.push_back(b);
+    }
+
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+
+    int distance = 0;
+
+    for (int i = 0; i < v1.size(); i++)
+    {
+        if (v1[i] != v2[i])
+        {
+            distance += abs(v1[i] - v2[i]);
+        }
+    }
+
+    cout << distance << endl;
+}
