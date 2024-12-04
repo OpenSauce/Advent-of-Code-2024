5f3e89a593ada090ab3219ed95918cc9d25d9a08
OpenSauce <lawleycobhc@gmail.com>
2024-12-04 09:32:53 +0000

    Day 4 task one

---
+++ 4/4.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int find_xmas(const vector<vector<char>> &matrix, int i, int j)
+{
+    int count = 0;
+    // Up
+    if (i >= 3 && matrix[i - 1][j] == 'M' && matrix[i - 2][j] == 'A' && matrix[i - 3][j] == 'S')
+    {
+        count++;
+    }
+
+    // Down
+    if (i < matrix.size() - 3 && matrix[i + 1][j] == 'M' && matrix[i + 2][j] == 'A' && matrix[i + 3][j] == 'S')
+    {
+        count++;
+    }
+
+    // Left
+    if (j >= 3 && matrix[i][j - 1] == 'M' && matrix[i][j - 2] == 'A' && matrix[i][j - 3] == 'S')
+    {
+        count++;
+    }
+
+    // Right
+    if (j < matrix[i].size() - 3 && matrix[i][j + 1] == 'M' && matrix[i][j + 2] == 'A' && matrix[i][j + 3] == 'S')
+    {
+        count++;
+    }
+
+    // Up-Left
+    if (i >= 3 && j >= 3 && matrix[i - 1][j - 1] == 'M' && matrix[i - 2][j - 2] == 'A' && matrix[i - 3][j - 3] == 'S')
+    {
+        count++;
+    }
+
+    // Up-Right
+    if (i >= 3 && j < matrix[i].size() - 3 && matrix[i - 1][j + 1] == 'M' && matrix[i - 2][j + 2] == 'A' && matrix[i - 3][j + 3] == 'S')
+    {
+        count++;
+    }
+
+    // Down-Left
+    if (i < matrix.size() - 3 && j >= 3 && matrix[i + 1][j - 1] == 'M' && matrix[i + 2][j - 2] == 'A' && matrix[i + 3][j - 3] == 'S')
+    {
+        count++;
+    }
+
+    // Down-Right
+    if (i < matrix.size() - 3 && j < matrix[i].size() - 3 && matrix[i + 1][j + 1] == 'M' && matrix[i + 2][j + 2] == 'A' && matrix[i + 3][j + 3] == 'S')
+    {
+        count++;
+    }
+
+    return count;
+}
+
+int task_one()
+{
+    vector<vector<char>> matrix;
+    ifstream input("input.txt");
+    for (string line; getline(input, line);)
+    {
+        matrix.push_back(vector<char>(line.begin(), line.end()));
+    }
+
+    int res = 0;
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            if (matrix[i][j] == 'X')
+            {
+                res += find_xmas(matrix, i, j);
+            }
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+    cout << task_one() << endl;
+    return 0;
+}
