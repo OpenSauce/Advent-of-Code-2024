29794e85511807d011d927b9d99810a763e9df1d
OpenSauce <lawleycobhc@gmail.com>
2024-12-04 09:39:53 +0000

    Day 4 task one

--- 4/4.cpp
+++ 4/4.cpp
@@ -82,8 +82,52 @@ int task_one()
     return res;
 }
 
+int find_x_mas(const vector<vector<char>> &matrix, int i, int j)
+{
+    // Check boundaries
+    if (i < 1 || i >= matrix.size() - 1 || j < 1 || j >= matrix[i].size() - 1)
+    {
+        return 0;
+    }
+
+    if ((matrix[i - 1][j - 1] == 'M' && matrix[i + 1][j + 1] == 'S' ||
+         matrix[i - 1][j - 1] == 'S' && matrix[i + 1][j + 1] == 'M') &&
+        (matrix[i - 1][j + 1] == 'M' && matrix[i + 1][j - 1] == 'S' ||
+         matrix[i - 1][j + 1] == 'S' && matrix[i + 1][j - 1] == 'M'))
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+int task_two()
+{
+    vector<vector<char>> matrix;
+    ifstream input("input.txt");
+    for (string line; getline(input, line);)
+    {
+        matrix.push_back(vector<char>(line.begin(), line.end()));
+    }
+
+    int res = 0;
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        for (int j = 0; j < matrix[i].size(); j++)
+        {
+            if (matrix[i][j] == 'A')
+            {
+                res += find_x_mas(matrix, i, j);
+            }
+        }
+    }
+
+    return res;
+}
+
 int main()
 {
     cout << task_one() << endl;
+    cout << task_two() << endl;
     return 0;
 }
